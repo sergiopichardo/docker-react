edaf35a23426e23589e1ec823ac86324dc2351a0
Sergio Pichardo <sp88work@gmail.com>
2020-05-03 09:03:46 -0500

    change app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
                 <p>
-                    Hola Mundo!
+                    Hello World!!!!!!! (change)
                 </p>
                 <a
                     className="App-link"
